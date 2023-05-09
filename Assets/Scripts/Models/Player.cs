87544980cc8403f4deb7e7524f535c87b96bc563
vic-meateater <victor.myasoedov@gmail.com>
2023-05-09 16:20:18 +0500

    ref code

---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace Bario
+{
+    public class Player
+    {
+        private readonly float _speed;
+        public Player(float speed)
+        {
+            _speed = speed;
+        }
+    }
+}
